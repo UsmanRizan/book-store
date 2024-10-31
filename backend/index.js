8e39f7c80c62e88856fdd8cfaff41601310a39a7
UsmanRizan <usmanrizan6@gmail.com>
2024-10-31 15:00:31 +0530

    Created API to update book by ID

--- backend/index.js
+++ backend/index.js
@@ -1,71 +1,90 @@
 import express from "express";
-import {PORT,mongoDBURL} from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
-import{Book} from "./models/bookModel.js";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    console.log(req);
-    return res.status(234).send("Hello World!");
-    });
+  console.log(req);
+  return res.status(234).send("Hello World!");
+});
 
 // route to save a bookModel
 app.post("/books", async (req, res) => {
-    try {
-        if (!req.body.title || !req.body.author || !req.body.publishYear) {
-            return res.status(400).send("Please fill all required fields");
-        }
-        const newBook = {
-            title: req.body.title,
-            author: req.body.author,
-            publishYear: req.body.publishYear,
-        };
-        const book = await Book.create(newBook);
-        return res.status(201).send(book);
-    } catch (error) {
-        console.error("Error occurred: ", error);
-        return res.status(500).send("Internal server error");
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res.status(400).send("Please fill all required fields");
     }
+    const newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      publishYear: req.body.publishYear,
+    };
+    const book = await Book.create(newBook);
+    return res.status(201).send(book);
+  } catch (error) {
+    console.error("Error occurred: ", error);
+    return res.status(500).send("Internal server error");
+  }
 });
 
 // route to get all books
 app.get("/books", async (req, res) => {
-    try {
-        const books = await Book.find({});
-        return res.status(200).json({
-            count : books.length,
-            data : books
-        });
-        
-    } catch (error) {
-        console.error("Error occurred: ", error);
-        return res.status(500).send("Internal server error");
-        
-}});
+  try {
+    const books = await Book.find({});
+    return res.status(200).json({
+      count: books.length,
+      data: books,
+    });
+  } catch (error) {
+    console.error("Error occurred: ", error);
+    return res.status(500).send("Internal server error");
+  }
+});
 
 // route to get a book by id
 app.get("/books/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const book = await Book.findById(id);
-        if (!book) {
-            return res.status(404).send("Book not found");
-        }
-        return res.status(200).send(book);
-    } catch (error) {
-        console.error("Error occurred: ", error);
-        return res.status(500).send("Internal server error");
-    }
+  try {
+    const { id } = req.params;
+
+    const book = await Book.findById(id);
+    return res.status(200).json(book);
+  } catch (error) {
+    console.error("Error occurred: ", error);
+    return res.status(500).send("Internal server error");
+  }
 });
 
-mongoose.connect(mongoDBURL).then(() => {
+// route to update a book by id
+app.put("/books/:id", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res.status(400).send("Please fill all required fields");
+    }
+    const { id } = req.params;
+    const result = await Book.findByIdAndUpdate(id, req.body);
+
+    if (!result) {
+      return res.status(404).send("Book not found");
+    }
+    return res.status(200).send("Book updated successfully");
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Internal server error");
+  }
+});
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
     console.log("Connected to the database");
     app.listen(PORT, () => {
-        console.log(`Server running on http://localhost:${PORT}`);
-      });
-  }).catch((err) => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
     console.error("Error connecting to the database: ", err);
   });
