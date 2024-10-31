90f2a41b697b7525803e7341fe69b18a69b30d6c
UsmanRizan <usmanrizan6@gmail.com>
2024-10-31 10:19:04 +0530

    connected to database

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+
+export const mongoDBURL = "mongodb+srv://root:root@book-store.5wyom.mongodb.net/book-collection?retryWrites=true&w=majority&appName=book-store";
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
-import {PORT} from "./config.js";
+import {PORT,mongoDBURL} from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -8,6 +9,13 @@ app.get("/", (req, res) => {
     return res.status(234).send("Hello World!");
     });
 
+    
+
+mongoose.connect(mongoDBURL).then(() => {
+    console.log("Connected to the database");
     app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+        console.log(`Server running on http://localhost:${PORT}`);
+      });
+  }).catch((err) => {
+    console.error("Error connecting to the database: ", err);
+  });
