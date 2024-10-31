67c097d8e37f3beb900f004727dae21fe24e842c
UsmanRizan <usmanrizan6@gmail.com>
2024-10-31 11:05:36 +0530

    Created API to create book

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,35 @@
 import express from "express";
 import {PORT,mongoDBURL} from "./config.js";
 import mongoose from "mongoose";
+import{Book} from "./models/bookModel.js";
 
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
     console.log(req);
     return res.status(234).send("Hello World!");
     });
 
-    
+// route to save a bookModel
+app.post("/books", async (req, res) => {
+    try {
+        if (!req.body.title || !req.body.author || !req.body.publishYear) {
+            return res.status(400).send("Please fill all required fields");
+        }
+        const newBook = {
+            title: req.body.title,
+            author: req.body.author,
+            publishYear: req.body.publishYear,
+        };
+        const book = await Book.create(newBook);
+        return res.status(201).send(book);
+    } catch (error) {
+        console.error("Error occurred: ", error);
+        return res.status(500).send("Internal server error");
+    }
+});
 
 mongoose.connect(mongoDBURL).then(() => {
     console.log("Connected to the database");
