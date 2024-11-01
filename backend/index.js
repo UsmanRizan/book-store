fffa99cb05ea545ce10fb1e1be243b939068085f
UsmanRizan <usmanrizan6@gmail.com>
2024-11-01 23:39:25 +0530

    CORS policy Update

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,21 @@ import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import bookRouter from "./routes/bookRoutes.js";
+import cors from "cors";
 
 const app = express();
 
+// Middleware for parsing request body
 app.use(express.json());
+
+// Middleware for enabling CORS
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 app.get("/", (req, res) => {
   console.log(req);
